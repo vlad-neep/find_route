144c9994c972e03161fb78a7e4518d45d655c28f
vlad <vdubovoy425@gmail.com>
2022-09-01 16:56:56 +0300

    first commit

--- travel/views.py
+++ travel/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def home(request):
     a = 'vladik'
     return render(request, 'home.html', {'a': a})
+
+def about(request):
+    return render(request, 'about.html')
